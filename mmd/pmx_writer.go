1988591fccdc87eda6aeb6a806095341cb147a66
Kousuke Kawahira <kawahira@binzume.net>
2020-01-25 23:43:27 +0900

    Add .pmx writer

---
+++ mmd/pmx_writer.go
@@ -0,0 +1,226 @@
+package mmd
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type baseWriter struct {
+	w io.Writer
+}
+
+func (p *baseWriter) write(v interface{}) error {
+	return binary.Write(p.w, binary.LittleEndian, v)
+}
+
+func (p *baseWriter) writeUint8(v uint8) uint8 {
+	binary.Write(p.w, binary.LittleEndian, &v)
+	return v
+}
+
+func (p *baseWriter) writeUint16(v uint16) uint16 {
+	binary.Write(p.w, binary.LittleEndian, &v)
+	return v
+}
+
+func (p *baseWriter) writeInt(v int) {
+	vv := int32(v)
+	binary.Write(p.w, binary.LittleEndian, &vv)
+}
+
+func (p *baseWriter) writeFloat(v float32) float32 {
+	binary.Write(p.w, binary.LittleEndian, &v)
+	return v
+}
+
+func (p *baseWriter) writeVUInt(sz byte, vv int) int {
+	if sz == 1 {
+		var v = uint8(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	if sz == 2 {
+		var v = uint16(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	if sz == 4 {
+		var v = uint32(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	return 0
+}
+
+func (p *baseWriter) writeVInt(sz byte, vv int) int {
+	if sz == 1 {
+		var v = int8(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	if sz == 2 {
+		var v = int16(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	if sz == 4 {
+		var v = int32(vv)
+		binary.Write(p.w, binary.LittleEndian, &v)
+		return int(v)
+	}
+	return 0
+}
+
+// PMXWriter is writer for .pmx data
+type PMXWriter struct {
+	baseWriter
+	header *Header
+}
+
+func (w *PMXWriter) Write(doc *PMXDocument) error {
+	// header
+	w.writeHeader(doc.Header)
+	w.writeText(doc.Name)
+	w.writeText(doc.NameEn)
+	w.writeText(doc.Comment)
+	w.writeText(doc.CommentEn)
+
+	// vertexes
+	w.writeInt(len(doc.Vertexes))
+	for _, v := range doc.Vertexes {
+		w.writeVertex(v)
+	}
+
+	// faces
+	w.writeInt(len(doc.Faces) * 3)
+	for _, f := range doc.Faces {
+		w.writeFace(f)
+	}
+
+	// textures
+	w.writeInt(len(doc.Textures))
+	for _, t := range doc.Textures {
+		w.writeText(t)
+	}
+
+	// materials
+	w.writeInt(len(doc.Materials))
+	for _, m := range doc.Materials {
+		w.writeMaterial(m)
+	}
+
+	// TODO
+	w.writeInt(0) // bones
+	w.writeInt(0) // morphs
+	w.writeInt(0)
+	w.writeInt(0)
+	w.writeInt(0)
+
+	return nil
+}
+
+func (w *PMXWriter) writeText(v string) {
+	w.writeInt(len(v))
+	binary.Write(w.w, binary.LittleEndian, []byte(v))
+}
+
+func (w *PMXWriter) writeIndex(attrTyp int, v int) {
+	w.writeVInt(w.header.Info[attrTyp], v)
+}
+
+func (w *PMXWriter) writeUIndex(attrTyp int, v int) {
+	w.writeVUInt(w.header.Info[attrTyp], v)
+}
+
+func (w *PMXWriter) writeHeader(h *Header) {
+	w.header = h
+
+	// TODO
+	h.Format = []byte("PMX ")
+	h.Info[AttrStringEncoding] = 1
+
+	w.write(&h.Format)
+	w.write(&h.Version)
+	w.writeUint8(uint8(len(h.Info)))
+	w.write(&h.Info)
+}
+
+func (w *PMXWriter) writeVertex(v *Vertex) {
+	w.write(&v.Pos)
+	w.write(&v.Normal)
+	w.write(&v.UV)
+	w.write(&v.ExtUVs)
+
+	var wehghtType uint8
+	switch len(v.BoneWeights) {
+	case 1:
+		wehghtType = 0
+		break
+	case 2:
+		wehghtType = 1
+		break
+	case 4:
+		wehghtType = 2
+		break
+	case 11:
+		wehghtType = 3
+		break
+	}
+	if len(v.BoneWeights) == 1 {
+		wehghtType = 0
+	} else if len(v.BoneWeights) == 2 {
+		wehghtType = 1
+	}
+
+	w.writeUint8(wehghtType)
+	for _, b := range v.Bones {
+		w.writeIndex(AttrBoneIndexSz, b)
+	}
+	if wehghtType == 0 {
+	} else if wehghtType == 1 {
+		w.writeFloat(v.BoneWeights[0])
+	} else if wehghtType == 3 {
+		w.writeFloat(v.BoneWeights[0])
+		w.write(v.BoneWeights[2:])
+	} else {
+		w.write(v.BoneWeights)
+	}
+	w.write(&v.EdgeScale)
+}
+
+func (w *PMXWriter) writeFace(f *Face) {
+	w.writeIndex(AttrVertIndexSz, f.Verts[0])
+	w.writeIndex(AttrVertIndexSz, f.Verts[1])
+	w.writeIndex(AttrVertIndexSz, f.Verts[2])
+}
+
+func (w *PMXWriter) writeMaterial(m *Material) {
+	w.writeText(m.Name)
+	w.writeText(m.NameEn)
+	w.write(&m.Color)
+	w.write(&m.Specular)
+	w.write(&m.Specularity)
+	w.write(&m.AColor)
+	w.write(&m.Flags)
+	w.write(&m.EdgeColor)
+	w.write(&m.EdgeScale)
+
+	w.writeIndex(AttrTexIndexSz, m.TextureID)
+	w.writeIndex(AttrTexIndexSz, m.EnvID)
+
+	w.write(&m.EnvMode)
+	w.write(&m.ToonType)
+	if m.ToonType == 0 {
+		w.writeIndex(AttrTexIndexSz, m.Toon)
+	} else {
+		w.writeUint8(uint8(m.Toon))
+	}
+
+	w.writeText(m.Memo)
+	w.writeInt(m.Count)
+}
+
+// WritePMX writes .pmx data
+func WritePMX(doc *PMXDocument, w io.Writer) error {
+	return (&PMXWriter{baseWriter: baseWriter{w}}).Write(doc)
+}
