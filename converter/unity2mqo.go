089f9d2980179388dcfacfe4a88147379c654fe5
Kousuke Kawahira <kawahira@binzume.net>
2021-09-20 20:35:58 +0900

    unity2mqo: texture scale

--- converter/unity2mqo.go
+++ converter/unity2mqo.go
@@ -24,7 +24,10 @@ type unityToMqoState struct {
 	UnityToMQOOption
 	src *unity.Scene
 	dst *mqo.Document
-	mat map[string]int
+	mat map[string]struct {
+		index   int
+		uvScale *geom.Vector2
+	}
 }
 
 func NewUnityToMQOConverter(options *UnityToMQOOption) *UnityToMQOConverter {
@@ -44,25 +47,30 @@ func (conv *UnityToMQOConverter) Convert(secne *unity.Scene) (*mqo.Document, err
 		UnityToMQOOption: *conv.options,
 		src:              secne,
 		dst:              mqo.NewDocument(),
-		mat:              map[string]int{},
+		mat: map[string]struct {
+			index   int
+			uvScale *geom.Vector2
+		}{},
 	}
 
 	// TODO
 	state.dst.Materials = append(state.dst.Materials, &mqo.Material{Name: "dummy", Color: geom.Vector4{X: 1, Y: 1, Z: 1, W: 1}})
 
 	for _, o := range secne.Objects {
-		state.convertObject(o, 0)
+		state.convertObject(o, 0, true)
 	}
 
 	return state.dst, nil
 }
 
-func (c *unityToMqoState) convertObject(o *unity.GameObject, d int) {
+func (c *unityToMqoState) convertObject(o *unity.GameObject, d int, active bool) {
 	dst := c.dst
 	obj := mqo.NewObject(o.Name)
+	active = active && o.IsActive != 0
 	dst.Objects = append(dst.Objects, obj)
 	obj.UID = len(dst.Objects)
 	obj.Depth = d
+	obj.Visible = active
 
 	tr := o.GetTransform()
 	if tr == nil {
@@ -71,54 +79,59 @@ func (c *unityToMqoState) convertObject(o *unity.GameObject, d int) {
 
 	var meshFilter *unity.MeshFilter
 	var meshRenderer *unity.MeshRenderer
-	if o.GetComponent(&meshFilter) && o.GetComponent(&meshRenderer) {
-		mat := 0
-		if len(meshRenderer.Materials) > 0 {
-			matGUID := meshRenderer.Materials[0].GUID
-			mat = c.mat[matGUID]
-			if mat == 0 {
-				mat = len(dst.Materials)
-				m := &mqo.Material{Name: matGUID, Color: geom.Vector4{X: 1, Y: 1, Z: 1, W: 1}, Diffuse: 0.8}
-				c.mat[matGUID] = mat
-				dst.Materials = append(dst.Materials, m)
-				material, err := unity.LoadMaterial(o.Scene.Assets, matGUID)
-				log.Println(material)
-				if err == nil {
-					if c := material.GetColorProperty("_Color"); c != nil {
-						m.Color = geom.Vector4{X: c.R, Y: c.G, Z: c.B, W: c.A}
-					}
-					m.Name = matGUID + "_" + material.Name
-					if t := material.GetTextureProperty("_MainTex"); t != nil && t.Texture.IsValid() {
-						texAsset := o.Scene.Assets.GetAsset(t.Texture.GUID)
-						if c.SaveTexrure && texAsset != nil {
-							m.Texture, err = c.saveTexrure(texAsset)
-							if err != nil {
-								log.Println(err)
-							}
-						}
-					}
-					if t := material.GetTextureProperty("_BumpMap"); t != nil && t.Texture.IsValid() {
-						texAsset := o.Scene.Assets.GetAsset(t.Texture.GUID)
-						if c.SaveTexrure && texAsset != nil {
-							m.BumpTexture, err = c.saveTexrure(texAsset)
-							if err != nil {
-								log.Println(err)
-							}
-						}
-					}
-				}
-			}
-		}
+	if o.GetComponent(&meshFilter) && o.GetComponent(&meshRenderer) && meshFilter.Mesh.IsValid() {
 		s := c.ConvertScale
 		trmat := geom.NewScaleMatrix4(s, s, s).Mul(tr.GetWorldMatrix())
 		if name, ok := unity.UnityMeshes[*meshFilter.Mesh]; ok {
+			mat := struct {
+				index   int
+				uvScale *geom.Vector2
+			}{}
+			if len(meshRenderer.Materials) > 0 {
+				matGUID := meshRenderer.Materials[0].GUID
+				mat = c.mat[matGUID]
+				if mat.index == 0 {
+					mat.index = len(dst.Materials)
+					m := &mqo.Material{Name: matGUID, Color: geom.Vector4{X: 1, Y: 1, Z: 1, W: 1}, Diffuse: 0.8}
+					material, err := unity.LoadMaterial(o.Scene.Assets, matGUID)
+					log.Println(material)
+					if err == nil {
+						if c := material.GetColorProperty("_Color"); c != nil {
+							m.Color = geom.Vector4{X: c.R, Y: c.G, Z: c.B, W: c.A}
+						}
+						m.Name = matGUID + "_" + material.Name
+						if t := material.GetTextureProperty("_MainTex"); t != nil && t.Texture.IsValid() {
+							texAsset := o.Scene.Assets.GetAsset(t.Texture.GUID)
+							if c.SaveTexrure && texAsset != nil {
+								m.Texture, err = c.saveTexrure(texAsset)
+								if err != nil {
+									log.Println(err)
+								}
+							}
+							mat.uvScale = &t.Scale
+						}
+						if t := material.GetTextureProperty("_BumpMap"); t != nil && t.Texture.IsValid() {
+							texAsset := o.Scene.Assets.GetAsset(t.Texture.GUID)
+							if c.SaveTexrure && texAsset != nil {
+								m.BumpTexture, err = c.saveTexrure(texAsset)
+								if err != nil {
+									log.Println(err)
+								}
+							}
+						}
+					}
+					dst.Materials = append(dst.Materials, m)
+					c.mat[matGUID] = mat
+				}
+			}
+
 			obj.Name += name
 			if name == "Cube" {
-				Cube(obj, trmat, mat)
+				Cube(obj, trmat, mat.index, mat.uvScale)
 			} else if name == "Plane" {
-				Plane(obj, trmat, mat)
+				Plane(obj, trmat, mat.index, mat.uvScale)
 			} else if name == "Quad" {
-				Quad(obj, trmat, mat)
+				Quad(obj, trmat, mat.index, mat.uvScale)
 			} else {
 				log.Println("TODO:", name)
 			}
@@ -126,13 +139,14 @@ func (c *unityToMqoState) convertObject(o *unity.GameObject, d int) {
 			asset := c.src.Assets.GetAsset(meshFilter.Mesh.GUID)
 			if asset != nil {
 				meta, _ := c.src.Assets.GetMetaFile(asset)
+
 				log.Println("TODO:", asset.Path, meta.GetRecycleNameByFileID(meshFilter.Mesh.FileID))
 			}
 		}
 	}
 
 	for _, child := range tr.GetChildren() {
-		c.convertObject(child.GetGameObject(), d+1)
+		c.convertObject(child.GetGameObject(), d+1, active)
 	}
 }
 
@@ -156,7 +170,7 @@ func (c *unityToMqoState) saveTexrure(texAsset *unity.Asset) (string, error) {
 	return fileName, err
 }
 
-func AddGeometry(o *mqo.Object, tr *geom.Matrix4, mat int, vs []*geom.Vector3, faces [][]int, uvs [][]geom.Vector2) {
+func AddGeometry(o *mqo.Object, tr *geom.Matrix4, mat int, vs []*geom.Vector3, faces [][]int, uvs [][]geom.Vector2, uvScale *geom.Vector2) {
 	voffset := len(o.Vertexes)
 	for _, v := range vs {
 		o.Vertexes = append(o.Vertexes, tr.ApplyTo(v))
@@ -168,12 +182,18 @@ func AddGeometry(o *mqo.Object, tr *geom.Matrix4, mat int, vs []*geom.Vector3, f
 		face := &mqo.Face{Material: mat, Verts: f}
 		if len(uvs) > n {
 			face.UVs = uvs[n]
+			if uvScale != nil {
+				for i := range face.UVs {
+					face.UVs[i].X *= uvScale.X
+					face.UVs[i].Y *= uvScale.Y
+				}
+			}
 		}
 		o.Faces = append(o.Faces, face)
 	}
 }
 
-func Cube(o *mqo.Object, tr *geom.Matrix4, mat int) {
+func Cube(o *mqo.Object, tr *geom.Matrix4, mat int, uvScale *geom.Vector2) {
 	vs := []*geom.Vector3{
 		{X: -0.5, Y: -0.5, Z: -0.5},
 		{X: 0.5, Y: -0.5, Z: -0.5},
@@ -197,26 +217,26 @@ func Cube(o *mqo.Object, tr *geom.Matrix4, mat int) {
 		{{X: 1, Y: 1}, {X: 1, Y: 0}, {X: 0, Y: 0}, {X: 0, Y: 1}},
 		{{X: 1, Y: 1}, {X: 1, Y: 0}, {X: 0, Y: 0}, {X: 0, Y: 1}},
 	}
-	AddGeometry(o, tr, mat, vs, faces, uvs)
+	AddGeometry(o, tr, mat, vs, faces, uvs, uvScale)
 }
 
-func Quad(o *mqo.Object, tr *geom.Matrix4, mat int) {
+func Quad(o *mqo.Object, tr *geom.Matrix4, mat int, uvScale *geom.Vector2) {
 	vs := []*geom.Vector3{
 		{X: -0.5, Y: -0.5},
 		{X: 0.5, Y: -0.5},
-		{X: 0.5, Y: 0.5},
 		{X: -0.5, Y: 0.5},
+		{X: 0.5, Y: 0.5},
 	}
 	faces := [][]int{
-		{0, 1, 2, 3},
+		{0, 1, 3, 2},
 	}
 	uvs := [][]geom.Vector2{
-		{{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 1, Y: 1}, {X: 0, Y: 1}},
+		{{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 0, Y: 1}, {X: 1, Y: 1}},
 	}
-	AddGeometry(o, tr, mat, vs, faces, uvs)
+	AddGeometry(o, tr, mat, vs, faces, uvs, uvScale)
 }
 
-func Plane(o *mqo.Object, tr *geom.Matrix4, mat int) {
+func Plane(o *mqo.Object, tr *geom.Matrix4, mat int, uvScale *geom.Vector2) {
 	vs := []*geom.Vector3{
 		{X: -5, Y: 0, Z: -5},
 		{X: 5, Y: 0, Z: -5},
@@ -229,5 +249,5 @@ func Plane(o *mqo.Object, tr *geom.Matrix4, mat int) {
 	uvs := [][]geom.Vector2{
 		{{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 1, Y: 1}, {X: 0, Y: 1}},
 	}
-	AddGeometry(o, tr, mat, vs, faces, uvs)
+	AddGeometry(o, tr, mat, vs, faces, uvs, uvScale)
 }
