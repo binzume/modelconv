7651c79c4c4b28a3b248709d8d4c270366159726
Kousuke Kawahira <kawahira@binzume.net>
2021-09-21 22:15:12 +0900

    unity: fix rotation

--- unity/unity.go
+++ unity/unity.go
@@ -131,7 +131,7 @@ type Transform struct {
 
 func (tr *Transform) GetMatrix() *geom.Matrix4 {
 	pos := geom.NewTranslateMatrix4(tr.LocalPosition.X, tr.LocalPosition.Y, -tr.LocalPosition.Z)
-	rot := geom.NewRotationMatrix4FromQuaternion(&tr.LocalRotation)
+	rot := geom.NewRotationMatrix4FromQuaternion(&geom.Vector4{X: tr.LocalRotation.X, Y: tr.LocalRotation.Y, Z: -tr.LocalRotation.Z, W: tr.LocalRotation.W})
 	sacle := geom.NewScaleMatrix4(tr.LocalScale.X, tr.LocalScale.Y, tr.LocalScale.Z)
 	return pos.Mul(rot).Mul(sacle)
 }
