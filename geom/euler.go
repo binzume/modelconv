f5dd6b69d32312ca243e1739e4a62dfe96e51587
Kousuke Kawahira <kawahira@binzume.net>
2022-01-22 02:49:01 +0900

    geom: add rotation orders

--- geom/euler.go
+++ geom/euler.go
@@ -6,7 +6,9 @@ type RotationOrder int
 
 const (
 	RotationOrderXYZ = iota
+	RotationOrderYXZ
 	RotationOrderZXY
+	RotationOrderZYX
 )
 
 type EulerAngles struct {
@@ -24,15 +26,9 @@ func NewEulerFromQuaternion(q *Quaternion, order RotationOrder) *EulerAngles {
 
 func NewEulerFromMatrix4(mat *Matrix4, order RotationOrder) *EulerAngles {
 	const eps = 0.00000001
-	m11 := float64(mat[0])
-	m21 := float64(mat[1])
-	m31 := float64(mat[2])
-	m12 := float64(mat[4])
-	m22 := float64(mat[5])
-	m32 := float64(mat[6])
-	m13 := float64(mat[8])
-	m23 := float64(mat[9])
-	m33 := float64(mat[10])
+	m11, m21, m31 := float64(mat[0]), float64(mat[1]), float64(mat[2])
+	m12, m22, m32 := float64(mat[4]), float64(mat[5]), float64(mat[6])
+	m13, m23, m33 := float64(mat[8]), float64(mat[9]), float64(mat[10])
 
 	ret := &EulerAngles{Order: order}
 	switch order {
@@ -46,6 +42,16 @@ func NewEulerFromMatrix4(mat *Matrix4, order RotationOrder) *EulerAngles {
 			ret.Z = 0
 		}
 		break
+	case RotationOrderYXZ:
+		ret.X = Element(math.Asin(-math.Max(-1, math.Min(m23, 1))))
+		if math.Abs(m23) < 1-eps {
+			ret.Y = Element(math.Atan2(m13, m33))
+			ret.Z = Element(math.Atan2(m21, m22))
+		} else {
+			ret.Y = Element(math.Atan2(-m31, m11))
+			ret.Z = 0
+		}
+		break
 	case RotationOrderZXY:
 		ret.X = Element(math.Asin(math.Max(-1, math.Min(m32, 1))))
 		if math.Abs(m32) < 1-eps {
@@ -54,6 +60,16 @@ func NewEulerFromMatrix4(mat *Matrix4, order RotationOrder) *EulerAngles {
 		} else {
 			ret.Z = Element(math.Atan2(m21, m11))
 			ret.Y = 0
+		}
+		break
+	case RotationOrderZYX:
+		ret.Y = Element(math.Asin(-math.Max(-1, math.Min(m31, 1))))
+		if math.Abs(m31) < 1-eps {
+			ret.X = Element(math.Atan2(m32, m33))
+			ret.Z = Element(math.Atan2(m21, m11))
+		} else {
+			ret.X = 0
+			ret.Z = Element(math.Atan2(-m12, m22))
 		}
 		break
 	}
@@ -75,12 +91,24 @@ func (v *EulerAngles) ToQuaternion() *Quaternion {
 			Y: float32(cx*sy*cz - sx*cy*sz),
 			Z: float32(cx*cy*sz + sx*sy*cz),
 			W: float32(cx*cy*cz - sx*sy*sz)}
+	case RotationOrderYXZ:
+		return &Vector4{
+			X: float32(sx*cy*cz + cx*sy*sz),
+			Y: float32(cx*sy*cz - sx*cy*sz),
+			Z: float32(cx*cy*sz - sx*sy*cz),
+			W: float32(cx*cy*cz + sx*sy*sz)}
 	case RotationOrderZXY:
 		return &Vector4{
 			X: float32(sx*cy*cz - cx*sy*sz),
 			Y: float32(cx*sy*cz + sx*cy*sz),
 			Z: float32(cx*cy*sz + sx*sy*cz),
 			W: float32(cx*cy*cz - sx*sy*sz)}
+	case RotationOrderZYX:
+		return &Vector4{
+			X: float32(sx*cy*cz - cx*sy*sz),
+			Y: float32(cx*sy*cz + sx*cy*sz),
+			Z: float32(cx*cy*sz - sx*sy*cz),
+			W: float32(cx*cy*cz + sx*sy*sz)}
 	default:
 		return &Quaternion{0, 0, 0, 1}
 	}
--- geom/euler_test.go
+++ geom/euler_test.go
@@ -16,11 +16,33 @@ func TestEulerXYZ(t *testing.T) {
 	}
 }
 
+func TestEulerYXZ(t *testing.T) {
+	const eps = 0.000001
+
+	e1 := NewEuler(10*math.Pi/180, 20*math.Pi/180, 30*math.Pi/180, RotationOrderYXZ)
+	e2 := NewEulerFromQuaternion(e1.ToQuaternion(), RotationOrderYXZ)
+
+	if e1.Vector3.Sub(&e2.Vector3).Len() > eps {
+		t.Error("euler: ", e1, e2)
+	}
+}
+
 func TestEulerZXY(t *testing.T) {
 	const eps = 0.000001
 
 	e1 := NewEuler(10*math.Pi/180, 20*math.Pi/180, 30*math.Pi/180, RotationOrderZXY)
 	e2 := NewEulerFromQuaternion(e1.ToQuaternion(), RotationOrderZXY)
+
+	if e1.Vector3.Sub(&e2.Vector3).Len() > eps {
+		t.Error("euler: ", e1, e2)
+	}
+}
+
+func TestEulerZYX(t *testing.T) {
+	const eps = 0.000001
+
+	e1 := NewEuler(10*math.Pi/180, 20*math.Pi/180, 30*math.Pi/180, RotationOrderZYX)
+	e2 := NewEulerFromQuaternion(e1.ToQuaternion(), RotationOrderZYX)
 
 	if e1.Vector3.Sub(&e2.Vector3).Len() > eps {
 		t.Error("euler: ", e1, e2)
